d545aa165b3f4e3552a1349ca70fee4bd3f685dd
Vijay <vijayjs2208@gmail.com>
2018-12-15 16:05:28 -0800

    branch to track minimal scaffold react project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const title = 'My Minimal React Webpack Babel Setup';
-
+// minimal React scaffold project
 ReactDOM.render(
     <div>{title}</div>,
     document.getElementById('app')
